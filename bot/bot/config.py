00338cf3267444fb45889fbabaea2b5aefc0cf10
Alexander <alex_step04@mail.ru>
2026-02-17 19:59:23 +0300

    del py bot

--- bot/bot/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-API_BASE_URL = os.getenv("API_BASE_URL", "http://backend:8080")
-BOT_API_KEY = os.getenv("BOT_API_KEY", "")
-POLL_INTERVAL = float(os.getenv("POLL_INTERVAL", "2"))
-TOKEN_REFRESH_INTERVAL = float(os.getenv("TOKEN_REFRESH_INTERVAL", "30"))
--- bot/bot/states/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-
-class QuizStates(StatesGroup):
-    enter_code = State()
-    enter_nickname = State()
-    change_nickname = State()
-    in_session = State()
